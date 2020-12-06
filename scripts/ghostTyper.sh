b37782d0b22b5c7feca6a7af58baa66ad9632124
Phil <pmash2@users.noreply.github.com>
2020-12-06 14:46:06 -0500

    Add more CLI options to ghostTyper

--- scripts/ghostTyper.sh
+++ scripts/ghostTyper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-"C:\Program Files (x86)\Notepad++\notepad++.exe" -qf"..\assets\reactCode.js" -qSpeed2 -ljavascript
+"C:\Program Files (x86)\Notepad++\notepad++.exe" -qf"..\assets\reactCode.js" -qSpeed2 -ljavascript -multiInst -nosession -notabbar
